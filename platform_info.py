4a7e6612d4d364f25329bd77c925ca5d1bd56540
Christian Clauss <cclauss@me.com>
2019-11-03 00:04:56 +0100

    CCC makes a commit

--- platform_info.py
+++ platform_info.py
@@ -14,29 +14,29 @@ for name, value in inspect.getmembers(platform):
 # print(sys.platform, sys.version)
 
 """
-         architecture() = ('64bit', '')     
+         architecture() = ('64bit', '')
               mac_ver() = ('11.0.3', ('', '', ''), 'iPad5,4')
               machine() = iPad5,4
                  node() = CCC-iPad
-             platform() = Darwin-17.0.0-iPad5,4-64bit                               
+             platform() = Darwin-17.0.0-iPad5,4-64bit
          python_build() = ('default', 'Aug 24 2017 16:20:00')
       python_compiler() = GCC 4.2.1 Compatible Apple LLVM 8.1.0 (clang-802.0.42)
 python_implementation() = CPython
-       python_version() = 3.6.1     
+       python_version() = 3.6.1
  python_version_tuple() = ('3', '6', '1')
               release() = 17.0.0
                system() = Darwin
-                uname() = uname_result(system='Darwin', node='CCC-iPad',                
+                uname() = uname_result(system='Darwin', node='CCC-iPad',
                                        release='17.0.0',
                                        version='Darwin Kernel Version 17.0.0: '
                                                'Fri Sep  1 14:59:13 PDT 2017; '
                                                'root:xnu-4570.2.5~167/'
                                                'RELEASE_ARM64_T7001',
-                                       machine='iPad5,4', processor='')                 
+                                       machine='iPad5,4', processor='')
               version() = ('Darwin Kernel Version 17.0.0: '
                            'Fri Sep  1 14:59:13 PDT 2017; '
-                           'root:xnu-4570.2.5~167/RELEASE_ARM64_T7001')          
+                           'root:xnu-4570.2.5~167/RELEASE_ARM64_T7001')
 
-ios 3.6.1 (default, Aug 24 2017, 16:20:00)          
+ios 3.6.1 (default, Aug 24 2017, 16:20:00)
 [GCC 4.2.1 Compatible Apple LLVM 8.1.0 (clang-802.0.42)]
 """
