66e5f336a2763c29408fbe97b618253c4a38d24e
elephantworks <elephantworks@gmail.com>
2019-11-02 17:57:30 -0400

    Update platform_info.py

--- platform_info.py
+++ platform_info.py
@@ -4,7 +4,7 @@ import platform
 for name, value in inspect.getmembers(platform): 
     if name[0] != "_" and callable(value):
         try:
-            value = value()         
+            value = value()                 
         except (IndexError, TypeError):
             continue
         if str(value).strip("(),' "):
@@ -14,7 +14,7 @@ for name, value in inspect.getmembers(platform):
 # print(sys.platform, sys.version)
 
 """
-         architecture() = ('64bit', '')
+         architecture() = ('64bit', '')     
               mac_ver() = ('11.0.3', ('', '', ''), 'iPad5,4')
               machine() = iPad5,4
                  node() = CCC-iPad
@@ -22,11 +22,11 @@ for name, value in inspect.getmembers(platform):
          python_build() = ('default', 'Aug 24 2017 16:20:00')
       python_compiler() = GCC 4.2.1 Compatible Apple LLVM 8.1.0 (clang-802.0.42)
 python_implementation() = CPython
-       python_version() = 3.6.1
+       python_version() = 3.6.1     
  python_version_tuple() = ('3', '6', '1')
               release() = 17.0.0
                system() = Darwin
-                uname() = uname_result(system='Darwin', node='CCC-iPad',
+                uname() = uname_result(system='Darwin', node='CCC-iPad',                
                                        release='17.0.0',
                                        version='Darwin Kernel Version 17.0.0: '
                                                'Fri Sep  1 14:59:13 PDT 2017; '
