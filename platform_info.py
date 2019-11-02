711d9c5e8ead47ebe2bbcecb6185c2ccbd8d9ef0
elephantworks <elephantworks@gmail.com>
2019-11-02 17:39:44 -0400

    Update platform_info.py

--- platform_info.py
+++ platform_info.py
@@ -4,11 +4,11 @@ import platform
 for name, value in inspect.getmembers(platform): 
     if name[0] != "_" and callable(value):
         try:
-            value = value()
+            value = value()         
         except (IndexError, TypeError):
             continue
         if str(value).strip("(),' "):
-            print("{:>21}() = {}".format(name, value)) 
+            print("{:>21}() = {}".format(name, value))
 
 # import sys
 # print(sys.platform, sys.version)
