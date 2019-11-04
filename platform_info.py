003a887f893b1f8f0bd9a1f1ccaabddb2fdcec9c
Ryan Lewis <ryandlewis.rl@gmail.com>
2019-11-04 11:06:40 -0600

    Update platform_info.py

--- platform_info.py
+++ platform_info.py
@@ -9,7 +9,7 @@ for name, value in inspect.getmembers(platform):
         except (IndexError, TypeError):
             continue
         if str(value).strip("(),' "):
-            print("{:>21}() = {}".format(name, value))
+            print('{:>21}() = {}'.format(name, value))
 
 # import sys
 # print(sys.platform, sys.version)
